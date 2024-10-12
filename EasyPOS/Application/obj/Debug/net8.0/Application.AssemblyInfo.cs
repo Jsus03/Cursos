77c70c3744c50e7c21ac48ea441c46890bffd4ef
Practicantes <platinointerns@gmail.com>
2024-10-12 11:11:26 -0600

    Se creo la carpeta primitives con tres archivos y se instalo MediatR

--- EasyPOS/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ EasyPOS/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d03b206dee88ee0316a3aaeeccc3b8953a6505a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

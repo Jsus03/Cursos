948ed08eee9a2f0bfaa4d7af0c788b5685071251
Practicantes <platinointerns@gmail.com>
2024-10-14 09:35:52 -0600

    Assembly.

--- EasyPOS/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ EasyPOS/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2184a22b6ea34212d8f52eb5a0f926d3dfc969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945993839954030e10d556aef3cfb835ab126c61")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

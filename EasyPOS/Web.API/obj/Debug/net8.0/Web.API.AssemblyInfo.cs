486cab17a344c5780ae50694d0df9d37587621d8
Practicantes <platinointerns@gmail.com>
2024-10-14 11:19:14 -0600

    Se creo la base para el nuget ErrorOr.

--- EasyPOS/Web.API/obj/Debug/net8.0/Web.API.AssemblyInfo.cs
+++ EasyPOS/Web.API/obj/Debug/net8.0/Web.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24228e98c47805c13515ca056cd5dc8f7f5997ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d9d2435dc097a3abdf88bdfe809e250807a8e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

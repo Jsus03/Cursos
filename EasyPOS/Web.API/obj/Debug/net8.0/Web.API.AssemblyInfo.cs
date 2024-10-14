4b521d49c730a865a08f9fc8e7b329d25574de1d
Practicantes <platinointerns@gmail.com>
2024-10-14 14:27:31 -0600

    Assembly

--- EasyPOS/Web.API/obj/Debug/net8.0/Web.API.AssemblyInfo.cs
+++ EasyPOS/Web.API/obj/Debug/net8.0/Web.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b1682a406d5284d2acc96843f969e55d3144b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a25b35a7baf31885aa091135b51703d94d6c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
